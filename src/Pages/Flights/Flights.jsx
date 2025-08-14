08e724cb007bcd8f73a14809234f0b591713448b
saranyes <iamsaranselvakumar@gmail.com>
2025-08-15 01:05:52 +0530

    flight

--- src/Pages/Flights/Flights.jsx
+++ src/Pages/Flights/Flights.jsx
@@ -8,10 +8,13 @@ import DatePicker from "react-datepicker";
 const Flights = () => {
 
     const now = new Date();
-    const day = now.toLocaleString("en-us", { weekday: "short" })
+    const day = now.toLocaleString("en-us", { weekday: "long" })
     const date = now.getDate();
-    const month = now.toLocaleString("en-us", { month: "short" })
+    const month = now.toLocaleString("en-us", { month: "long" })
+    const year = now.getFullYear();
 
+    const [nowdate, setnowdate] = useState(false)
+    const togglenowdate = () => setnowdate(prev => !prev)
 
     const [roundtrip, setroundtrip] = useState(false)
     return (
@@ -20,8 +23,8 @@ const Flights = () => {
             <div className="relative w-screen">
                 <img src={flightbg} alt="bg" className="w-full h-auto opacity-50" />
 
-                <div className="absolute inset-0 flex flex-col items-center  gap-10 z-10">
-                    <ul className="flex font-semibold gap-100 pt-10">
+                <div className="absolute inset-0 flex flex-col items-center gap-10 z-10">
+                    <ul className="flex font-semibold gap-20 pt-10">
                         <li>
                             <input
                                 type="checkbox"
@@ -41,16 +44,20 @@ const Flights = () => {
                     <div className="flex flex-wrap  mt-4  bg-white drop-shadow-xl drop-shadow-gray-400 outline-1 outline-gray-100 rounded-xl font-semibold">
                         <div className="flex flex-col px-4 w-100 py-2">
                             FROM
-                            <h1>Delhi</h1>
+                            <input type="searchbar" />
 
                         </div>
                         <div className="flex flex-col px-4 w-100 py-2">
                             TO
                             <h1>Bangaluru</h1>
+                            <input type="searchbar" />
                         </div>
-                        <div className="flex flex-col px-4 w-100 py-2">
-                            DEPARTURE
-                            <input type="date" className="flex" />
+                        <div className="flex flex-col px-4 w-100 py-2 cursor-pointer">
+                            <h1 className="cursor-pointer" onClick={togglenowdate} >DEPARTURE</h1>
+                            <h1 onClick={togglenowdate} className="cursor-pointer">{date}/{day}/{year}</h1>
+                            {nowdate && (
+                                <input type="date" className="flex" />
+                            )}
                         </div>
                         {roundtrip && (
                             <div className="flex flex-col px-4 w-100  py-2">
