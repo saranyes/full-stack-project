ef2fa5aa40cfaf5bfb8e288d861497e8cb174bf4
saranyes <iamsaranselvakumar@gmail.com>
2025-08-19 09:11:01 +0530

    flight api in progress

---
+++ src/Components/Displayflights.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Displayflights = () => {
+    return (
+        <div>
+            Results
+
+        </div>
+    )
+}
+
+export default Displayflights
--- src/Pages/Flights/Flights.jsx
+++ src/Pages/Flights/Flights.jsx
@@ -1,17 +1,11 @@
 
 import Navbar from "../../Components/Navbar"
-import { use, useState } from "react"
+import { useState } from "react"
 import flightbg from "../../assets/flightbg.jpg";
-import DatePicker from "react-datepicker";
+import Displayflights from "../../Components/Displayflights";
 
 
 const Flights = () => {
-
-    const now = new Date();
-    const day = now.toLocaleString("en-us", { weekday: "long" })
-    const date = now.getDate();
-    const month = now.toLocaleString("en-us", { month: "long" })
-    const year = now.getFullYear();
 
     const [nowdate, setnowdate] = useState(false)
     const togglenowdate = () => setnowdate(prev => !prev)
@@ -20,57 +14,62 @@ const Flights = () => {
     return (
         <div>
             <Navbar />
-            <div className="relative w-screen">
-                <img src={flightbg} alt="bg" className="w-full h-auto opacity-50" />
+            <div className="relative ">
+                <div>  <img src={flightbg} alt="bg" className="w-screen h-auto opacity-50" /></div>
 
-                <div className="absolute inset-0 flex flex-col items-center gap-10 z-10">
-                    <ul className="flex font-semibold gap-20 pt-10">
-                        <li>
-                            <input
-                                type="checkbox"
-                                checked={!roundtrip}
-                                onChange={() => setroundtrip(false)}
-                            /> One-Way
-                        </li>
-                        <li>
-                            <input
-                                type="checkbox"
-                                checked={roundtrip}
-                                onChange={() => setroundtrip(true)}
-                            /> Round-Trip
-                        </li>
-                    </ul>
 
-                    <div className="flex flex-wrap  mt-4  bg-white drop-shadow-xl drop-shadow-gray-400 outline-1 outline-gray-100 rounded-xl font-semibold">
-                        <div className="flex flex-col px-4 w-100 py-2">
-                            FROM
-                            <input type="searchbar" />
+                <div className="absolute inset-0 flex flex-col items-center gap-10   ">
+                    <div>
+                        <ul className="flex gap-20 pt-10 ">
+                            <li>
+                                <input
+                                    type="checkbox"
+                                    checked={!roundtrip}
+                                    onChange={() => setroundtrip(false)}
+                                /> One-Way
+                            </li>
+                            <li>
+                                <input
+                                    type="checkbox"
+                                    checked={roundtrip}
+                                    onChange={() => setroundtrip(true)}
+                                /> Round-Trip
+                            </li>
+                        </ul>
 
-                        </div>
-                        <div className="flex flex-col px-4 w-100 py-2">
-                            TO
-                            <h1>Bangaluru</h1>
-                            <input type="searchbar" />
-                        </div>
-                        <div className="flex flex-col px-4 w-100 py-2 cursor-pointer">
-                            <h1 className="cursor-pointer" onClick={togglenowdate} >DEPARTURE</h1>
-                            <h1 onClick={togglenowdate} className="cursor-pointer">{date}/{day}/{year}</h1>
-                            {nowdate && (
-                                <input type="date" className="flex" />
-                            )}
-                        </div>
-                        {roundtrip && (
-                            <div className="flex flex-col px-4 w-100  py-2">
-                                RETURN
+                        <div className="flex flex-wrap  mt-4  bg-white  outline-1  rounded-xl font-semibold ">
+                            <div className="flex flex-col px-4  py-2  ">
+                                FROM
+                                <input type="searchbar " className="outline-2 outline-gray-300 rounded-xl px-2" />
+
+                            </div>
+                            <div className="flex flex-col px-4 wpt-2">
+                                TO
+                                <input type="searchbar  " className="outline-2 outline-gray-300 rounded-xl px-2" />
+                            </div>
+                            <div className="flex flex-col px-4  py-2 cursor-pointer">
+                                <h1 className="cursor-pointer" onClick={togglenowdate} >DEPARTURE</h1>
+
+
                                 <input type="date" className="flex" />
 
                             </div>
-                        )}
+                            {roundtrip && (
+                                <div className="flex flex-col px-4   py-2">
+                                    RETURN
+                                    <input type="date" className="flex" />
+                                </div>
+                            )}
+                        </div>
+
+                        <div className="flex justify-center"><button className="px-20 bg-blue-400 flex justify-center items-center py-2 rounded-xl cursor-pointer">Search</button></div>
                     </div>
+                    <Displayflights />
+
                 </div>
             </div>
+        </div >
 
-        </div>
     )
 }
 
